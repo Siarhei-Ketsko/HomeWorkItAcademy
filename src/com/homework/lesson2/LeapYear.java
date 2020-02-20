06dd6974492b85b5b793aed279e8ceeadcba7c97
Siarhei-Ketsko <sergey.klaks@gmail.com>
2020-02-20 19:57:31 +0300

    update

--- src/com/homework/lesson2/LeapYear.java
+++ src/com/homework/lesson2/LeapYear.java
@@ -1,7 +1,30 @@
 package com.homework.lesson2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class LeapYear {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(reader.readLine());
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println(year + " Год високосный");
+                }
+                else {
+                    System.out.println(year + " Год не високосный");
+                }
+            }
+            else {
+                System.out.println(year + " Год високосный");
+            }
+        }
+        else {
+            System.out.println(year + " Год не високосный");
+        }
 
 
     }

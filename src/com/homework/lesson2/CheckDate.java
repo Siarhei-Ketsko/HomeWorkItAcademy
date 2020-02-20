7ee03e327c23f5f303205bceb2ad38af46c9bf23
Siarhei-Ketsko <sergey.klaks@gmail.com>
2020-02-20 21:46:50 +0300

    update

--- src/com/homework/lesson2/CheckDate.java
+++ src/com/homework/lesson2/CheckDate.java
@@ -6,7 +6,56 @@ import java.io.InputStreamReader;
 
 public class CheckDate {
     public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int day = Integer.parseInt(reader.readLine());
+        int month = Integer.parseInt(reader.readLine());
+        int year = Integer.parseInt(reader.readLine());
+        int xDay = 0;
 
+
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                xDay = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                xDay = 30;
+                break;
+            case 2:
+                if (year % 4 == 0) {
+                    if (year % 100 == 0) {
+                        if (year % 400 == 0) {
+                            xDay = 29;
+                        }
+                        else {
+                            xDay = 28;
+                        }
+                    }
+                    else {
+                        xDay = 29;
+                    }
+                }
+                else {
+                    xDay = 28;
+                }
+                break;
+
+        }
+
+        if (day > xDay) {
+            System.out.println("Даты не существует");
+        }
+        else {
+            System.out.println("Дата существует");
+        }
 
     }
 }

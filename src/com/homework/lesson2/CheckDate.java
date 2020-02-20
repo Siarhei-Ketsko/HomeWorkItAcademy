fa7add5804f88800c3b2e8f1fca129dfe941f55e
Siarhei-Ketsko <sergey.klaks@gmail.com>
2020-02-20 19:21:01 +0300

    update

--- src/com/homework/lesson2/CheckDate.java
+++ src/com/homework/lesson2/CheckDate.java
@@ -3,5 +3,6 @@ package com.homework.lesson2;
 public class CheckDate {
     public static void main(String[] args) {
 
+
     }
 }
--- src/com/homework/lesson2/Rubli.java
+++ src/com/homework/lesson2/Rubli.java
@@ -8,11 +8,19 @@ public class Rubli {
     public static void main(String [] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int summa = Integer.parseInt(reader.readLine());
+        int secondLastNumber = summa % 100 / 10;
 
-        if (summa % 10 >= 2 && summa % 10 <=4) System.out.println(summa + " Рубля");
-        else if (summa % 10 == 1) System.out.println(summa + " Рубль");
-        else if (summa % 10 >=5 && summa % 10 <=9 || summa % 10 == 0) System.out.println(summa + " Рублей");
+        if (secondLastNumber == 1) {
+            System.out.println(summa + " Рублей");
+        }
+        else if (summa % 10 >=2 && summa % 10 <=4) {
+            System.out.println(summa + " Рубля");
+        }
 
+        else  if (summa == 1) {
+            System.out.println(summa + " Рубль");
+        }
+        else System.out.println(summa + " Рублей");
 
     }
 

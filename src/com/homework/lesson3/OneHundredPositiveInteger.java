828c7d2e4076246c77124b2035c1df94b045c6d2
Siarhei <sergey.klaks@gmail.com>
2020-02-24 12:43:10 +0300

    update

--- src/com/homework/lesson3/OneHundredPositiveInteger.java
+++ src/com/homework/lesson3/OneHundredPositiveInteger.java
@@ -5,10 +5,15 @@ public class OneHundredPositiveInteger {
         int[] arr = new int[100];
         int number = 1;
         for (int i = 0; i <  arr.length; i++) {
-
-        }
+         while (number % 13 != 0 && number %17 != 0) {
+             number++;
+         }
+         arr[i] = number;
+         number++;
+         }
         for (int i = 0; i < arr.length; i++ ) {
             System.out.println(arr[i]);
         }
     }
 }
+

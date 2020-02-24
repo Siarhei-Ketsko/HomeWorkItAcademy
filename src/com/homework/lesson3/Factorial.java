2894a874bd0350060efc1c49e99c5a44f8d73bae
Siarhei <sergey.klaks@gmail.com>
2020-02-24 19:19:12 +0300

    update

--- src/com/homework/lesson3/Factorial.java
+++ src/com/homework/lesson3/Factorial.java
@@ -8,40 +8,35 @@ public class Factorial {
         System.out.println("Введите 1 если хотите посчитать циклом \"FOR\" или 2 циклом \"WHILE \"");
         int number = sc.nextInt();
         int result = 1;
-        int y = 1;
-        int n = 0;
+        int i = 1;
         if (number == 1) {
             System.out.println("Введите число");
             number = sc.nextInt();
-            result = goForCycle(number, result);
+            result = goForCycle(number, result, i);
             printResult(result, number);
         }
 
         else if (number ==2){
             System.out.println("Введите число");
             number = sc.nextInt();
-            result = goWhileCycle(number, result, y, n);
+            result = goWhileCycle(number, result, i);
             printResult(result, number);
         }
         else {
             System.out.println("Ошибка, вы ввели неверное число, перезапустите программу");
         }
-
-
     }
 
-    public static int goWhileCycle(int number, int result, int y, int n) {
-        while (n<number) {
-            result *= y;
-            y++;
-            n++;
+    public static int goWhileCycle(int number, int result, int i) {
+        while (i<=number) {
+            result *= i;
+            i++;
         }
         return result;
     }
 
-    public static int goForCycle(int number, int result) {
-        for (int i = 1; i <=number; i++){
-
+    public static int goForCycle(int number, int result, int i) {
+        for (; i <=number; i++){
             result *= i;
         }
         return result;

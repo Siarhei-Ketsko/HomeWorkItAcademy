3ea2813ef60153bfa4d5aaeb8d89ac982ba8251e
Siarhei <sergey.klaks@gmail.com>
2020-02-25 23:28:37 +0300

    update

--- src/com/homework/lesson3/EqualsMultidimensionalArray.java
+++ src/com/homework/lesson3/EqualsMultidimensionalArray.java
@@ -3,34 +3,22 @@ package com.homework.lesson3;
 import java.util.Scanner;
 
 public class EqualsMultidimensionalArray {
+
+    static int[][] firstArray = {{0, 1, 2, 3, 4}, {5, 6, 7, 8, 9}};
+    static int[][] secondArray = {{0, 1, 2, 3, 4}, {1, 1, 7, 8, 9}};
+
     public static void main(String[] args) {
-        int[][] firstArray = {{0, 1, 2, 3, 4}, {5, 6, 7, 8, 9}};
-        int[][] secondArray = {{0, 2, 2, 3, 4}, {1, 1, 7, 8, 9}};
-        String [][] fStringArray = {{"а", "а","в"}, {"а", "б", "в"}};
-        String [][] sStringArray = {{"а", "а","в"}, {"а", "б", "в"}};
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите индексы элементов двумерных массивов через пробел в формате \"i j i j\"");
-        int indexI = sc.nextInt();
-        int indexJ = sc.nextInt();
-        int indexItwo = sc.nextInt();
-        int indexJtwo = sc.nextInt();
-
-
-
-        equals(indexI,indexJ,indexItwo,indexJtwo,firstArray,secondArray,fStringArray,sStringArray);
-
+        if (equals(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt())) {
+            System.out.println("Элементы равны");
+        }
+        else {
+            System.out.println("Элементы не равны");
+        }
     }
-
-    public static void equals(int indexI, int indexJ, int indexItwo, int indexJtwo, int[][]firstArray, int[][]secondArray, String[][]fStringArray, String[][]sStringArray) {
-        if (firstArray[indexI][indexJ] == secondArray[indexItwo][indexJtwo]) {
-            System.out.println("Элементы int массива Равны");
-        } else {
-            System.out.println("Элементы int массива не равны");
-        }
-        if (fStringArray[indexI][indexJ].equals(sStringArray[indexItwo][indexJtwo])) {
-            System.out.println("Элементы String массива Равны");
-        } else {
-            System.out.println("Элементы String массива не равны");
-        }
+    public static boolean equals(int indexI, int indexJ, int indexItwo, int indexJtwo) {
+        return firstArray[indexI][indexJ] == secondArray[indexItwo][indexJtwo];
     }
 }
+

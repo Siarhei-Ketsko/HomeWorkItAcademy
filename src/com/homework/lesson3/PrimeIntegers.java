cd69134c0e0deabab486004d77d19b8bcb1b2947
Siarhei <sergey.klaks@gmail.com>
2020-02-25 12:00:35 +0300

    update

--- src/com/homework/lesson3/PrimeIntegers.java
+++ src/com/homework/lesson3/PrimeIntegers.java
@@ -5,24 +5,38 @@ import java.util.Scanner;
 public class PrimeIntegers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int number;
+        int i = 1;
+        int count = 1;
         while (true) {
-            int number = sc.nextInt();
-            if (number == 0) {
+            System.out.println("Введите число, для выхода из программы введите \"0\"");
+            number = sc.nextInt();
+            if(number == 0) {
                 break;
             }
-            if (number == 1) {
-                System.out.println("Число не простое");
-            }
-            else if (number == 2) {
-                    System.out.println("Число простое");
-                }
-            else if (number % 1 == 0 && number % number == 0 && number % 2 != 0) {
-                System.out.println("Число " + number + " простое");
-            } else {
-                System.out.println("Число не простое");
+            count = getCount(number, i, count);
+            printResult(count, number);
+            count = 1;
+        }
+    }
+
+    public static int getCount(int number, int i, int count) {
+        while (i<number) {
+            i++;
+            if (number%i == 0) {
+                count++;
             }
         }
+        return count;
+    }
 
+    public static void printResult(int count, int number) {
+        if (count == 2) {
+            System.out.println("Число " + number + " простое");
+        }
+        else {
+            System.out.println("Число " + number + " не простое");
+        }
     }
 }
 

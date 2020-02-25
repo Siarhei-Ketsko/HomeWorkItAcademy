993596cf23ea652beb8ec1f39fe4dde67b0a88cb
Siarhei <sergey.klaks@gmail.com>
2020-02-25 16:16:24 +0300

    update

--- src/com/homework/lesson3/EqualsMultidimensionalArray.java
+++ src/com/homework/lesson3/EqualsMultidimensionalArray.java
@@ -6,20 +6,27 @@ public class EqualsMultidimensionalArray {
     public static void main(String[] args) {
         int[][] firstArray = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}};
         int[][] secondArray = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}};
+        String [][] fStringArray = {{"а", "б","в"}, {"а", "б", "в"}};
+        String [][] sStringArray = {{"а", "б","в"}, {"а", "б", "в"}};
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите индексы элементов двумерных массивов через пробел в формате \"i j i j\"");
-        int elemOne = firstArray[sc.nextInt()][sc.nextInt()];
-        int elemTwo = secondArray[sc.nextInt()][sc.nextInt()];
+        int indexI = sc.nextInt();
+        int indexJ = sc.nextInt();
 
-        equals(elemOne, elemTwo);
+
+        equals(indexI,indexJ,firstArray,secondArray,fStringArray,sStringArray);
     }
 
-    public static void equals(int elemOne, int elemTwo) {
-        if (elemOne == elemTwo) {
-            System.out.println("Равны");
+    public static void equals(int indexI, int indexJ, int[][]firstArray, int[][]secondArray, String[][]fStringArray, String[][]sStringArray) {
+        if (firstArray[indexI][indexJ] == secondArray[indexI][indexJ]) {
+            System.out.println("Элементы int массива Равны");
+        } else {
+            System.out.println("Элементы int массива не равны");
         }
-        else {
-            System.out.println("Не равны");
+        if (fStringArray[indexI][indexJ].equals(sStringArray[indexI][indexJ])) {
+            System.out.println("Элементы String массива Равны");
+        } else {
+            System.out.println("Элементы String массива не равны");
         }
     }
 }

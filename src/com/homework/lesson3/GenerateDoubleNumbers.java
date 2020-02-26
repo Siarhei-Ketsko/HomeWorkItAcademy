26f58a7699921e3f500be07636f9ab566c2e0aa1
Siarhei <sergey.klaks@gmail.com>
2020-02-26 11:58:55 +0300

    update

--- src/com/homework/lesson3/GenerateDoubleNumbers.java
+++ src/com/homework/lesson3/GenerateDoubleNumbers.java
@@ -4,19 +4,27 @@ import java.util.Random;
 
 public class GenerateDoubleNumbers {
     public static void main(String[] args) {
-        double[] arr = new double[1000];
+        double[] arr = new double[10];
         Random random = new Random();
         double sum = 0.0;
+        double minumum = Integer.MAX_VALUE;
+        double maximum = Integer.MIN_VALUE;
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextDouble()*100;
             sum += arr[i];
-        }
-        Arrays.sort(arr);
 
-        System.out.println("Минимальное " + arr[0]);
+            if(arr[i] < minumum) {
+                minumum = arr[i];
+            }
+            if (arr[i] > maximum) {
+                maximum = arr[i];
+            }
+        }
+
+        System.out.println("Минимальное " + minumum);
         System.out.println("Среднее " + sum/arr.length);
-        System.out.println("Максимальное " + arr[arr.length-1]);
+        System.out.println("Максимальное " + maximum);
     }
 }
 

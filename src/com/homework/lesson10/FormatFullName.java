9e37ecb7945252a90040ca208b041dc1b0f4e8ed
Siarhei <sergey.klaks@gmail.com>
2020-03-11 19:55:11 +0300

    update

--- src/com/homework/lesson10/FormatFullName.java
+++ src/com/homework/lesson10/FormatFullName.java
@@ -3,11 +3,11 @@ package com.homework.lesson10;
 
 public class FormatFullName {
     public static void main(String[] args) {
-        System.out.println(formatFullName("Сергей","Кецко", "Сергеевич"));
+        System.out.println(formatFullName("Сергей", "Кецко", "Сергеевич"));
     }
 
     private static String formatFullName(String name, String secondName, String patronymic) {
-    String a = secondName.charAt(0) +"."+ name.charAt(0) +"."+ patronymic.charAt(0);
+        String a = secondName.charAt(0) + "." + name.charAt(0) + "." + patronymic.charAt(0);
         return a.toUpperCase();
     }
 }

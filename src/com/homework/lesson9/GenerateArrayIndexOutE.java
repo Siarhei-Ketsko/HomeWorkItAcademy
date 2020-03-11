506b880e4d54af936117d9cba16f9bf2ce5e40c1
Siarhei <sergey.klaks@gmail.com>
2020-03-11 12:48:39 +0300

    update

--- src/com/homework/lesson9/GenerateArrayIndexOutE.java
+++ src/com/homework/lesson9/GenerateArrayIndexOutE.java
@@ -7,11 +7,11 @@ public class GenerateArrayIndexOutE {
     private static final Logger LOGGER = Logger.getLogger(GenerateArrayIndexOutE.class);
 
     public static void main(String[] args) {
+        int[] arr = new int[4];
         try {
-            generateException();
-           /* int[] arr = new int[4];
+          //  generateException();
             arr[4] = 12;
-            */
+
         } catch (ArrayIndexOutOfBoundsException e) {
             LOGGER.error("Выход за пределы массива " + e.getMessage(), e);
         }

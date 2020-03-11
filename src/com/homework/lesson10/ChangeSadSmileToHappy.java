d9f1f6af7c1ce397d487366c9c74eafbdfbf73b2
Siarhei <sergey.klaks@gmail.com>
2020-03-11 16:49:52 +0300

    update

---
+++ src/com/homework/lesson10/ChangeSadSmileToHappy.java
@@ -0,0 +1,14 @@
+package com.homework.lesson10;
+
+public class ChangeSadSmileToHappy {
+    private static String test = "Тестируем:(замену :) смайликов:( в строке :(";
+
+    public static void main(String[] args) {
+
+        System.out.println(changeSmile(test));
+
+    }
+    private static String changeSmile(String str) {
+        return str.replace(":(", ":)");
+    }
+}

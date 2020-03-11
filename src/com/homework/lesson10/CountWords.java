00512135d77a01389357c08c71f3b640bff8500b
Siarhei <sergey.klaks@gmail.com>
2020-03-11 20:57:47 +0300

    update

--- src/com/homework/lesson10/CountWords.java
+++ src/com/homework/lesson10/CountWords.java
@@ -2,14 +2,14 @@ package com.homework.lesson10;
 
 public class CountWords {
     private static int cout;
-    public static String text = "Один два     три    ";
+    public static String text = "Один два     три   ";
 
     public static void main(String[] args) {
 
         System.out.println(countWords(text));
     }
     public static int countWords(String text) {
-        String[] words = text.split("\\s*(\\s|,|!|\\.)\\s*");
+        String[] words = text.split("\\s*(\\s|,|@|#|%|!|\\.)\\s*");
 
         for (int i = 0; i < words.length; i++) {
             cout++;

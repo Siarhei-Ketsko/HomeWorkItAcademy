4a57604bce2fa070b81da7ae2e86364e743af457
Siarhei <sergey.klaks@gmail.com>
2020-03-12 13:46:13 +0300

    update

--- src/com/homework/lesson10/FormatterIntegerNumber.java
+++ src/com/homework/lesson10/FormatterIntegerNumber.java
@@ -1,14 +1,17 @@
 package com.homework.lesson10;
 
+import java.util.Formatter;
+
 public class FormatterIntegerNumber {
     public static void main(String[] args) {
         System.out.println(formatOne(22));
     }
 
-    public static int formatOne(int number) {
+    public static String formatOne(int number) {
+        Formatter ftm = new Formatter();
+        String.
 
 
-
-        return 0;
+        return "0";
     }
 }

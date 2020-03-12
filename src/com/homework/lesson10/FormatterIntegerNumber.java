f5e049f65293dabd5a873ae3e2b48361df06601d
Siarhei <sergey.klaks@gmail.com>
2020-03-12 15:56:13 +0300

    update

--- src/com/homework/lesson10/FormatterIntegerNumber.java
+++ src/com/homework/lesson10/FormatterIntegerNumber.java
@@ -1,18 +1,31 @@
 package com.homework.lesson10;
 
-import java.util.Formatter;
 
 public class FormatterIntegerNumber {
+    private static long start;
+    private static long end;
+
     public static void main(String[] args) {
         System.out.println(formatOne(553));
     }
 
     public static String formatOne(int number) {
+        start = System.currentTimeMillis();
+
         String numberParse = Integer.toString(number);
+
         String newNumber = "0000000000" + number;
+
         String numberString = newNumber.substring(newNumber.length() - (newNumber.length() - numberParse.length()));
 
+        end = System.currentTimeMillis() - start;
+        System.out.println("Время выполнения первого способа: " + end + " ms");
 
         return numberString;
+    }
+    public static String formatTwo(int number) {
+
+
+        return "f";
     }
 }

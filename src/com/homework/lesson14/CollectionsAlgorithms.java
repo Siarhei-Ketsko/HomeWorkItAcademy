425608081ae1f66e0ce78d82771f7af8ee552398
Siarhei <sergey.klaks@gmail.com>
2020-03-20 09:50:24 +0300

    update

---
+++ src/com/homework/lesson14/CollectionsAlgorithms.java
@@ -0,0 +1,27 @@
+package com.homework.lesson14;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*
+Использовать Алгоритмы. Не используя вспомогательных объектов,
+переставить отрицательные элементы сгенерированного списка в конец,
+а положительные – в начало этого списка.
+ */
+
+
+public class CollectionsAlgorithms {
+
+    public static void main(String[] args) {
+
+        List<Integer> algorithms = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            algorithms.add((int) (Math.random()*50)-15);
+        }
+        Collections.sort(algorithms);
+
+        System.out.println(algorithms);
+    }
+
+}

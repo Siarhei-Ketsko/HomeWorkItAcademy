7bbeece3153c149297c8c0fff02cfcca62e66d5f
Siarhei <sergey.klaks@gmail.com>
2020-03-20 15:30:33 +0300

    update

---
+++ src/com/homework/lesson14/FrequencyDictionary.java
@@ -0,0 +1,65 @@
+package com.homework.lesson14;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/*
+
+* Имеется текст. Следует составить для него частотный словарь.
+*
+* */
+
+
+public class FrequencyDictionary {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Введите слова которые хотите добавить в словарь для поиска и нажмите Enter %n" +
+                "Скопируйте и вставьте текст или введите и нажмите Enter %n");
+        System.out.println(getFrequency(createLibrary(sc.nextLine()), StringSplitWords(sc.nextLine())));
+
+
+    }
+
+    public static String[] StringSplitWords(String str) {
+
+        String[] words = str.split("\\s*(\\s|,|!|\\?|\"|\\.)\\s*");
+
+        return words;
+    }
+
+    public static Map<String, Integer> createLibrary(String str) {
+
+        String [] spl = str.split("\\s*(\\s|,|!|\\?|\"|\\.)\\s*");
+
+        Map<String, Integer> library = new HashMap<>();
+
+        for (int i = 0; i < spl.length; i++) {
+
+            library.put(spl[i],0);
+        }
+
+        return library;
+    }
+
+
+    public static Map<String, Integer> getFrequency(Map<String, Integer> map, String[] str) {
+
+
+        for (int i = 0; i < str.length; i++) {
+            for (Map.Entry<String, Integer> word : map.entrySet()) {
+                if (str[i].equalsIgnoreCase(word.getKey())) {
+                    word.setValue(word.getValue() + 1);
+                }
+
+            }
+        }
+
+        return map;
+    }
+
+}

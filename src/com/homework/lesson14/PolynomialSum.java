d5cafe2c22c8d17c27832dafb3c55a0a2cec8f12
Siarhei <sergey.klaks@gmail.com>
2020-03-20 23:52:54 +0300

    update

--- src/com/homework/lesson14/PolynomialSum.java
+++ src/com/homework/lesson14/PolynomialSum.java
@@ -1,4 +1,15 @@
 package com.homework.lesson14;
 
+import java.util.Map;
+
 public class PolynomialSum {
+
+
+    public static void addPolynominal (Map<Integer, Integer> one, Map<Integer, Integer> two) {
+
+
+
+
+    }
+
 }

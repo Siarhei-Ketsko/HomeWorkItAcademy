e15f7ee0c3e623a8d718ad1b52b32b80ceae9767
Siarhei <sergey.klaks@gmail.com>
2020-03-20 22:06:46 +0300

    update

---
+++ src/com/homework/lesson14/UniqueNumber.java
@@ -0,0 +1,50 @@
+package com.homework.lesson14;
+
+import java.util.*;
+
+/*
+*
+*
+*   Написать метод countUnique, который принимает целочисленный список
+*   в качестве параметра и возвращает количество уникальных целых чисел в этом списке.
+*   При получении пустого списка метод должен возвращать 0.
+*   Пример:
+*   [3, 7, 3, -1, 2, 3, 7, 2, 15, 15] вернёт 5.
+*
+*
+* */
+
+
+
+public class UniqueNumber {
+
+    public static int[] arr = new int[]{3, 7, 3, -1, 2, 3, 7, 2, 15, 15};
+
+    public static int count;
+
+    public static void main(String[] args) {
+
+        List list = new ArrayList(11);
+
+
+      for (int i = 0; i < arr.length; i++) {
+
+         list.add(arr[i]);
+      }
+
+        System.out.println(countUnique(list));
+
+    }
+
+    public static int countUnique(List<Integer> arr) {
+
+        Set<Integer> set = new HashSet<>(arr);
+
+        for (Integer st : set) {
+            count++;
+        }
+
+        return count;
+    }
+
+}

1323e5ffed199051d9d88924e77dbca79e781680
Siarhei <sergey.klaks@gmail.com>
2020-03-21 17:02:01 +0300

    update

--- src/com/homework/lesson14/PolynomialSum.java
+++ src/com/homework/lesson14/PolynomialSum.java
@@ -1,13 +1,32 @@
 package com.homework.lesson14;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class PolynomialSum {
+
+    public static void main(String[] args) {
+
+        Map<Integer, Integer> polyOne = new HashMap<>();
+        polyOne.put(3, -5); polyOne.put(2, -1);
+        Map<Integer, Integer> polyTwo = new HashMap<>();
+        polyTwo.put(1,3); polyTwo.put(2,3);
+
+        addPolynominal(polyOne, polyTwo);
+
+
+    }
 
 
     public static void addPolynominal (Map<Integer, Integer> one, Map<Integer, Integer> two) {
 
 
+
+        for (Map.Entry<Integer, Integer> firstPoly : one.entrySet()) {
+
+
+
+        }
 
 
     }

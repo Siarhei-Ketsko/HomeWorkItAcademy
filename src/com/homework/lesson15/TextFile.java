f8a157ff7a4a3a4bc4e73826a9d0349c5f82a6eb
Siarhei <sergey.klaks@gmail.com>
2020-03-23 12:19:51 +0300

    update

---
+++ src/com/homework/lesson15/TextFile.java
@@ -0,0 +1,17 @@
+package com.homework.lesson15;
+
+public class TextFile {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static void getLineByLine(String text) {
+
+
+
+    }
+
+}

3e69ab7e828f583e248b9dc6baf08725dce0fd23
Siarhei <sergey.klaks@gmail.com>
2020-03-24 17:28:32 +0300

    update

---
+++ src/com/homework/lesson15/WriteIntToBinary.java
@@ -0,0 +1,114 @@
+package com.homework.lesson15;
+
+import java.io.*;
+
+/*
+*
+*Записать в двоичный файл 20 случайных чисел типа int со значением больше 255.
+* Прочитать записанный файл, распечатать числа и их среднее арифметическое.
+*
+* */
+
+public class WriteIntToBinary {
+
+    private static int[] arr = new int[]{256, 257, 258, 259, 260, 270, 280, 256, 259, 259, 352, 644, 277, 568, 277, 290, 257, 444, 523, 876};
+
+    public static void main(String[] args) {
+
+        File file = new File(File.separator + "Users" + File.separator + "klaks" + File.separator + "Documents" + File.separator + "Java IT-ACADEMY cours" +
+                File.separator + "JD 1 new" + File.separator + "Lection 15 Потоки ввода вывода" + File.separator + "IntToBinary.txt");
+
+
+        File fileTwo = new File(File.separator + "Users" + File.separator + "klaks" + File.separator + "Documents" + File.separator + "Java IT-ACADEMY cours" +
+                File.separator + "JD 1 new" + File.separator + "Lection 15 Потоки ввода вывода" + File.separator + "intToBinaryTwo.txt");
+
+
+        // testDataOutPut(file);
+        // testDataInput(file);
+        bitShift(fileTwo);
+        readBitShift(fileTwo);
+
+    }
+
+
+    public static void bitShift(File file) {
+
+        try (FileOutputStream out = new FileOutputStream(file)) {
+
+            for (int i = 0; i < arr.length; i++) {
+
+                out.write((arr[i] >>> 24) & 0xFF);
+                out.write((arr[i] >>> 16) & 0xFF);
+                out.write((arr[i] >>> 8) & 0xFF);
+                out.write((arr[i] >>> 0) & 0xFF);
+            }
+        } catch (Exception e) {
+
+
+        }
+
+    }
+
+    public static void readBitShift(File file) {
+
+        int[] testArr = new int[20];
+
+        try (FileInputStream input = new FileInputStream(file)) {
+
+            for (int i = 0; i < testArr.length; i++) {
+
+                int ch1 = input.read();
+                int ch2 = input.read();
+                int ch3 = input.read();
+                int ch4 = input.read();
+                if ((ch1 | ch2 | ch3 | ch4) < 0) {
+                    throw new EOFException();
+                }
+                testArr[i] = ((ch1 << 24) + (ch2 << 16) + (ch3 << 8) + (ch4 << 0));
+
+                System.out.println(testArr[i]);
+
+            }
+
+        } catch (Exception e) {
+
+
+        }
+
+    }
+
+   /* public static void testDataOutPut(File file) {
+
+        try (DataOutputStream out = new DataOutputStream(new FileOutputStream(file))) {
+            for (int i = 0; i < arr.length; i++) {
+
+                out.writeInt(arr[i]);
+            }
+
+
+        } catch (Exception e) {
+
+        }
+
+    }
+
+
+    public static void testDataInput(File file) {
+
+        int[] testArr = new int[20];
+
+        try (DataInputStream inputStream = new DataInputStream(new FileInputStream(file))) {
+            for (int i = 0; i < testArr.length; i++) {
+
+                testArr[i] = inputStream.readInt();
+                System.out.println(testArr[i]);
+            }
+
+
+        } catch (Exception e) {
+
+        }
+
+    }*/
+
+}

ed502f9bbc44a026c876733e42669aefeb0ef54e
Siarhei <sergey.klaks@gmail.com>
2020-03-30 13:54:46 +0300

    update

--- src/com/homework/lesson17/StreamsLambdas.java
+++ src/com/homework/lesson17/StreamsLambdas.java
@@ -16,37 +16,64 @@ public class StreamsLambdas {
             list.add(arr[i]);
         }
 
+        List<String> strings = new ArrayList<>();
+
+        strings.add("Тестстроки1");
+        strings.add("Тестстроки1");
+        strings.add("Тестстроки2");
+        strings.add("Тестст3");
+        strings.add("Тестст5");
+
+
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("1234567", 1);
+        map.put("123456", 2);
+        map.put("12345", 3);
+        map.put("123445678", 4);
+        map.put("123", 5);
+        map.put("fd", 1);
+
+        getAverage(list);
+
+        getUniqueStrings(strings);
+
+        getSumValues(map);
+
+        getConcatString(list);
+
+
+    }
+
+    private static void getAverage(List<Integer> list) {
+
         double average = list.stream().filter(x -> x % 5 == 0).collect(Collectors.averagingInt(Integer::intValue));
 
         System.out.println(average);
 
-        Arrays.stream(arr).filter(x -> x  % 5 == 0).average().stream().forEach(System.out::println);
+    }
 
-//////////////////////////////////////////////////////////////////////////////
-
-        List<String> strings = new ArrayList<>();
-
-        strings.add("Тестстроки1"); strings.add("Тестстроки1"); strings.add("Тестстроки2"); strings.add("Тестст3"); strings.add("Тестст5");
+    private static void getUniqueStrings(List<String> strings) {
 
         strings.stream().filter(x -> x.length() > 8).collect(Collectors.toSet()).forEach(System.out::println);
 
+    }
 
-//////////////////////////////////////////////////////////////////////////////
+    private static void getSumValues(Map<String, Integer> map) {
 
-        Map<String, Integer> map = new HashMap<>();
-
-        map.put("1234567",1); map.put("123456",2); map.put("12345", 3); map.put("123445678", 4); map.put("123",5); map.put("fd", 1);
-
-    int sum = map.entrySet().stream()
-              .filter(x -> x.getKey().length() < 7)
-              .map(Map.Entry::getValue)
-              .reduce((acc, x) -> x + acc).orElse(0);
+        int sum = map.entrySet().stream()
+                .filter(x -> x.getKey().length() < 7)
+                .map(Map.Entry::getValue)
+                .reduce((acc, x) -> x + acc).orElse(0);
 
         System.out.println(sum);
 
-//////////////////////////////////////////////////////////////////////////////
 
-    String result = list.stream().map(Object::toString).reduce((x, y) -> x + "" + y).orElse("");
+    }
+
+    private static void getConcatString(List<Integer> list) {
+
+        String result = list.stream().map(Object::toString).reduce((x, y) -> x + "" + y).orElse("");
         System.out.println(result);
 
     }

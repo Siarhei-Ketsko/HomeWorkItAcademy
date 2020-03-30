b5718d4b0dea2aac6ede069b8bce04617a5de561
Siarhei <sergey.klaks@gmail.com>
2020-03-30 23:00:15 +0300

    update

--- src/com/homework/lesson17/StreamsLambdas.java
+++ src/com/homework/lesson17/StreamsLambdas.java
@@ -5,6 +5,9 @@ import java.util.stream.Collectors;
 
 public class StreamsLambdas {
 
+    private static long start;
+    private static long end;
+
     private static int[] arr = new int[]{13, 5, 43, 42, 15, 26, 18, 67, 25};
 
     public static void main(String[] args) {
@@ -47,34 +50,106 @@ public class StreamsLambdas {
 
     private static void getAverage(List<Integer> list) {
 
-        double average = list.stream().filter(x -> x % 2 != 0 && x % 5 == 0).collect(Collectors.averagingInt(Integer::intValue));
+        start = System.currentTimeMillis();
 
-        System.out.println(average);
+        double average = list.stream()
+                .filter(number -> number % 2 != 0 && number % 5 == 0)
+                .collect(Collectors.averagingInt(Integer::intValue));
+
+        end = System.currentTimeMillis() - start;
+
+        System.out.println(average + " v1 " + end + " ms");
+
+        start = System.currentTimeMillis();
+
+        double averageTwo = list.stream()
+                .filter(number -> number % 2 != 0 && number % 5 == 0)
+                .mapToLong(number -> number)
+                .average()
+                .orElse(0);
+
+        end = System.currentTimeMillis() - start;
+
+        System.out.println(averageTwo + " v2 " + end + " ms" + "\n");
 
     }
 
     private static void getUniqueStrings(List<String> strings) {
 
-        strings.stream().filter(x -> x.length() > 8).collect(Collectors.toSet()).forEach(System.out::println);
+        start = System.currentTimeMillis();
+
+        strings.stream()
+                .filter(x -> x.length() > 8)
+                .collect(Collectors.toSet())
+                .forEach(System.out::println);
+
+        end = System.currentTimeMillis() - start;
+
+        System.out.println(" v1 " + end + " ms");
+
+        start = System.currentTimeMillis();
+
+        strings.stream()
+                .filter(x -> x.length() > 8)
+                .distinct()
+                .forEach(System.out::println);
+
+        end = System.currentTimeMillis() - start;
+
+        System.out.println(" v2 " + end + " ms" + "\n");
 
     }
 
     private static void getSumValues(Map<String, Integer> map) {
 
+        start = System.currentTimeMillis();
+
         int sum = map.entrySet().stream()
                 .filter(x -> x.getKey().length() < 7)
-                .map(Map.Entry::getValue)
+                .mapToInt(Map.Entry::getValue)
                 .reduce((acc, x) -> x + acc).orElse(0);
 
-        System.out.println(sum);
+        end = System.currentTimeMillis() - start;
+
+        System.out.println(sum + " v1 " + end + " ms");
+
+        start = System.currentTimeMillis();
+
+        int sumTwo = map.entrySet().stream()
+                .filter(x -> x.getKey().length() < 7)
+                .mapToInt(Map.Entry::getValue)
+                .sum();
+
+        end = System.currentTimeMillis() - start;
+
+        System.out.println(sumTwo + " v2 " + end + " ms" + "\n");
+
 
 
     }
 
     private static void getConcatString(List<Integer> list) {
 
-        String result = list.stream().map(String::valueOf).reduce((x, y) -> x + y).orElse("");
-        System.out.println(result);
+        start = System.currentTimeMillis();
+
+        String result = list.stream()
+                .map(String::valueOf)
+                .reduce((x, y) -> x + y)
+                .orElse("");
+
+        end = System.currentTimeMillis() - start;
+
+        System.out.println(result + " v1 " + end + " ms");
+
+        start = System.currentTimeMillis();
+
+        String resultTwo = list.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+
+        end = System.currentTimeMillis() - start;
+
+        System.out.println(resultTwo + " v2 " + end + " ms");
 
     }
 
